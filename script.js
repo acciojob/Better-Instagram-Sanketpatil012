514cfaa91b7a146b41e55963a5895885f4fc5c9f
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-03-15 16:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,57 +1,42 @@
-let arr = ['div1' , 'div2' , 'div3' , 'div4' , 'div5' , 'div6']
-const imageList= document.querySelectorAll('.image')
- let k=0; 
-  for(let imageSection of imageList)
-	 {
-          imageSection.id= arr[k]
-		  k++
 
-		 imageSection.addEventListener( 'dragstart' , (e)=>{
-			   let div= e.target
-			   div.style.opacity='0.5'
-			   e.dataTransfer.setData('text' , div.id)
-			 
-		 })
-
-		 imageSection.addEventListener( 'dragend' , (e)=>{
-			   let div=e.target 
-			   div.style.opacity='1'
-		 })
-	 
-	 }
-  let  allEvents = [  'dragover'   , 'dragenter' ,'drop']
-
- for(let  events of allEvents){
-
-    for(let imageSection of imageList){
-
-	   imageSection.addEventListener(events , (e)=>{
+let arr = ['div1', 'div2', 'div3', 'div4', 'div5', 'div6']
+let images = document.querySelectorAll('.image')
+let k = 0
+for (let imgSection of images) {
+    imgSection.id = arr[k]
+    k++
+ 
+    imgSection.addEventListener('dragstart', (e) => {
+        let div = e.target
+        div.style.opacity = '0.5'
+        e.dataTransfer.setData('text', div.id)
+    })
+ 
+    imgSection.addEventListener('dragend', (e) => {
+        let div = e.target
+        div.style.opacity = '1'
+    })
+}
+ 
+let eventsArr = ['dragover', 'dragenter', 'drop']
+for (let dragEvents of eventsArr) {
+    for (let imgSection of images) {
+        imgSection.addEventListener(dragEvents, (e) => {
             e.preventDefault()
-		   if(events === 'drop'){
-                 let drag1= e.dataTransfer.getData('text')
-                 let dragElement= document.getElementById(drag1) 
-			     let drag2 = imageSection.id 
-			     imageSection.id= drag1
-			     dragElement.id=drag2
-
-			     let text1= dragElement.innerText
-			     let text2=  imageSection.id
-			     dragElement.innerText=text1
-			      imageSection.id=text2
-			     
-		   }
-
-		   
-	   })
-  
-
-		
-	}
-	 
- }
-
-
-
+            if(dragEvents=='drop'){
+                let drag1 = e.dataTransfer.getData('text')
+                let drag1Element = document.getElementById(drag1)
+                let drag2 = imgSection.id
+                imgSection.id = drag1
+                drag1Element.id = drag2
+                let text1 = drag1Element.innerText
+                let text2 = imgSection.innerText
+                drag1Element.innerText = text2
+                imgSection.innerText = text1
+            }
+        })
+    }
+}
 
 
 
